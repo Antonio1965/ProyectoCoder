2a4f68e8fc684841cecaa24aee0088df85517dd8
TSUTSUMI José Antonio <64294956+Antonio1965@users.noreply.github.com>
2023-01-24 00:20:44 -0300

    Creacion de urls

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def inicio(resquest):
+  return HttpResponse("visita inicio")
+
+def cursos(resquest):
+  return HttpResponse("vista cursos")
+
+def profesores(resquest):
+  return HttpResponse("vistaprofesores")
+
+def estudiantes(resquest):
+  return HttpResponse("vista estudiantes")
+
+def entregables(resquest):
+  return HttpResponse("vistaentregables")
